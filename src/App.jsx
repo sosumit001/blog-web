eb89dd70c2c6b622cc0bc2c4dbe457a14af21160
sosumit001 <sosumit001@gmail.com>
2023-12-31 09:06:58 +0530

    ../

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
    
     <div  className="app-container">
-        <h2>This Webpage is shited to </h2>
+        <h2>This Webpage is shifted to </h2>
         <a style={{fontSize:'20px', color:'black'}} href="https://sumitso.in/">follow this link</a>
     </div>
 
